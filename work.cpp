8b1d1d78e7bc3a3303b4be7be8a18dc092e6cb9d
stanislavk285 <51738413+stanislavk285@users.noreply.github.com>
2023-12-24 19:35:19 +0300

    Create work.cpp

---
+++ work.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+//Пузырьковая сортировка
+void bubbleSort(double arr[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                // Обмен значениями
+                double temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+};
+int main(){
+    setlocale(LC_ALL,"rus");
+    int size;
+    cout << "Размер массива: ";
+    cin >> size;
+    if (size <= 0 || size > 10000) {
+        cout << "Размер массива должен быть положительным и не превышать 1000." <<endl;
+        return 0;
+    };
+    double arr[size];
+    // Ввод массива
+    for (int i = 0; i < size; ++i) {
+        cin >> arr[i];
+    };
+    //Сортировка массива с поиском наибольших элементов
+    bubbleSort(arr, size);
+    cout << "Два наибольших элемента массива: " << arr[size-1] << " индекс : " << size-1 << " и " << arr[size-2] << " индекс: " << size-2;
+return 0;
+}
